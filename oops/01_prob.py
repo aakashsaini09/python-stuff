6db99f00f70ce2db084672fbc01279cfd13c40f0
aakashsaini09 <aakashsaini948585@gmail.com>
2026-02-12 22:28:44 +0530

    abstraction

--- oops/01_prob.py
+++ oops/01_prob.py
@@ -3,10 +3,23 @@ class Car:
     def __init__(self, userbrand, usermodel):
         self.model = usermodel
         self.brand = userbrand
+    def fullName(self):
+        return f"Brought a car of {self.brand} company and {self.model} model"
+
+
+class Electric_car(Car):
+    def __init__(self, userbrand, usermodel, battery_size):
+        super().__init__(userbrand, usermodel)
+        self.battery_size = battery_size
+
+
 
 my_car = Car("toyota", "Corolla")
-print(my_car.brand)
 print(my_car.model)
 
 my_new_car = Car("Tata", "Safari")
 print(my_new_car.brand)
+print(my_new_car.fullName())
+
+electric_c = Electric_car("Tesla", "Monster", "10000mh")
+print(electric_c.brand, electric_c.fullName(), electric_c.battery_size)
