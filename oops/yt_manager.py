470aa58c9f7dae124ed0cc458233fd21c15bb0a7
aakashsaini09 <aakashsaini948585@gmail.com>
2026-02-14 23:08:10 +0530

    yt manager

--- oops/yt_manager.py
+++ oops/yt_manager.py
@@ -1,5 +1,18 @@
+import json
+def load_data():
+    try:
+        with open('youtube.txt', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data_helper(videos):
+    with open('youtube.txt','w') as file:
+        json.dump(videos, file)
+
 def list_all_videos(videos):
-    pass
+    for index, videos in enumerate(videos, start=1):
+        print(f"{index}. ")
 
 def add_video(videos):
     pass
@@ -8,24 +21,28 @@ def update_video(videos):
     pass
 def delete_video(videos):
     pass
-videos = []
-while True:
-    print("\n Youtube Manager | choose an option ")
-    print("1. List all youtube videos ")
-    print("2. Add a youtube video ")
-    print("3. Update a youtube video details ")
-    print("4. Delete a youtube video ")
-    print("5. Exit the app ")
-    choice = input("Enter your choice: ")
+def main():
+    videos = load_data()
+    while True:
+        print("\n Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice: ")
 
-    match choice:
-        case '1':
-            list_all_videos(videos)
-        case '2':
-            add_video(videos)
-        case '3':
-            update_video(videos)
-        case '4':
-            delete_video(videos)
-        case '5':
-            break
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+
+if __name__ == "__main__":
+    main()
