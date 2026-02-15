ca7936d82371bb749a0a80e3e569c2aa06edd949
aakashsaini09 <aakashsaini948585@gmail.com>
2026-02-15 13:01:33 +0530

    python terminal proj done

--- oops/yt_manager.py
+++ oops/yt_manager.py
@@ -11,16 +11,37 @@ def save_data_helper(videos):
         json.dump(videos, file)
 
 def list_all_videos(videos):
-    for index, videos in enumerate(videos, start=1):
-        print(f"{index}. ")
+    print('\n')
+    print('*' * 50)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. {video['name']}, Duration: {video['time']}")
+    print('\n')
+    print('*' * 50)
 
 def add_video(videos):
-    pass
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name': name, 'time': time})
+    save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update: "))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name: ")
+        time = input("Enter the new video time: ")
+        videos[index-1] = {'name': name, 'time': time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted: "))
+    if 1 <= index <= len(videos):
+        del videos[index - 1]
+        save_data_helper(videos)
+    else: 
+        print("Invalid video index")
 def main():
     videos = load_data()
     while True:
@@ -31,7 +52,7 @@ def main():
         print("4. Delete a youtube video ")
         print("5. Exit the app ")
         choice = input("Enter your choice: ")
-
+        # print(videos)
         match choice:
             case '1':
                 list_all_videos(videos)
