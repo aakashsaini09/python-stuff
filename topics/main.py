e7f69444901d14b438f4ef7d341f1db865dc0f49
aakashsaini09 <aakashsaini948585@gmail.com>
2026-06-24 14:19:15 +0530

    longest str qstn

--- topics/main.py
+++ topics/main.py
@@ -1,14 +1,14 @@
-# print("Hello world")
-# class Node: 
-#     def __init__(self, data=None, next=None):
-#         self.data = data
-#         self.next = next
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
+print("Hello world")
+class Node: 
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-# if __name__ == "__main__":
-#     pass
+if __name__ == "__main__":
+    pass
 
 # factorial
 def fact(num):
@@ -21,3 +21,31 @@ def fact(num):
 
 result = fact(5);
 print(result)
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if(len(s) == 0):
+            return 0
+        ans = 1
+        temp = ''
+        for i in s:
+            print("temp len: ", int(len(temp)))
+            if(int(len(temp)) > 0):
+                for j in temp:
+                    print("j: ", j)
+                    if(j == i):
+                        if(ans < int(len(temp))):
+                            ans = len(temp)
+                        temp = ''
+                        continue
+            temp = temp+i
+            print("ans: ", ans)
+            print("Temp: ", temp)
+            if(ans < int(len(temp))):
+                ans = len(temp)
+            print("********************")
+        return ans
+strings = Solution()
+ans1 = strings.lengthOfLongestSubstring("tabcabcbb")
+ans2 = strings.lengthOfLongestSubstring("au")
+print(ans1, ans2)
