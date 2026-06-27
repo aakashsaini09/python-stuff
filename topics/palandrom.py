4d1a4645cbef0b3c5cb0a413acaee8f4392dd72c
aakashsaini09 <aakashsaini948585@gmail.com>
2026-06-27 18:14:07 +0530

    longest palandrom solution

--- topics/palandrom.py
+++ topics/palandrom.py
@@ -1,4 +1,57 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        return s
+        ans = ''
+        match = False
+        for i in range(len(s)):
+            if(s[i]) == s[i + 2]:
+                match = True
+                while(match == True):
+                    print("match found: ", s[i], s[i+1], s[i+2])
+                    ans = s[i]+s[i+1]+s[i+2]
+                    for j in range(len(s)):
+                        ans = s[j]
+            else:
+                match = False
+        return ans
+    def longestP(self, s: str) -> str:
+        ans = ''
+        left=''
+        right=''
+        for i in range(len(s) - 1):
+            if(i == 0):
+                continue;
+            left = s[i - 1]
+            right = s[i + 1]
+            if left == right:
+                ans = left + s[i] + right
+
+        return ans
+    
+    def lpSolution(self, s: str) -> str:
+        if len(s) <=1:
+            return s
+        ml = 1
+        ms = s[0]
+        for i in range(len(s) - 1):
+            for j in range (i+1, len(s)):
+                if j-i+1 > ml and s[i:j+1] == s[i:j+1][::-1]:
+                    ml = j - i + 1
+                    ms = s[i:j+1]
+        return ms
+v1 = Solution()
+# v2 = v1.longestP('Aakash')
+# print("Ans: \n", v2)
+
+class Solution1:
+    def reverse(self, x: int) -> int:
+        ans = 0
+        # print("x: ", x) 
+        if(len(x) == 1):
+            ans = x
+        else:
+            ans = reversed(x)
+        return ans
+
+vi = Solution1()
+ans = vi.reverse(231)
+print("Ans: ", ans)
