8858b27037299628591c1b5399d558f31e124f7e
aakashsaini09 <aakashsaini948585@gmail.com>
2026-06-29 21:04:32 +0530

    palandrom prob done

--- topics/palandrom.py
+++ topics/palandrom.py
@@ -1,18 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        ans = ''
-        match = False
-        for i in range(len(s)):
-            if(s[i]) == s[i + 2]:
-                match = True
-                while(match == True):
-                    print("match found: ", s[i], s[i+1], s[i+2])
-                    ans = s[i]+s[i+1]+s[i+2]
-                    for j in range(len(s)):
-                        ans = s[j]
-            else:
-                match = False
-        return ans
+        if len(s) <= 1:
+            return s
+
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        max_str = s[0]
+
+        for i in range(len(s) - 1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i + 1)
+
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
+
+        return max_str
     def longestP(self, s: str) -> str:
         ans = ''
         left=''
---
+++ topics/zigzac.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        for i in range(len(s)):
+            
+            print(i, s[i])
+c = Solution()
+c.convert("chey", 4)
