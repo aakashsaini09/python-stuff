bbb0adce905d9f5f4256ff592b865c1dbe83ca45
aakashsaini09 <aakashsaini948585@gmail.com>
2026-07-05 17:55:00 +0530

    zigZac Prob solved

--- topics/zigzac.py
+++ topics/zigzac.py
@@ -1,7 +1,30 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        for i in range(len(s)):
-            
-            print(i, s[i])
+        row = 0
+        dir = 'down'
+        res = ''
+        list = [''] * numRows
+        if(numRows == 1):
+            return s
+        st = s.replace(" ", "")
+        for i in range(len(st)):
+            if(row == 0):
+                dir = 'down'
+            if(row == numRows - 1):
+                dir = 'up'
+            if(dir == 'down' and row < numRows):
+                list[row] += st[i]
+            elif(dir == 'up' and row > 0):
+                list[row] += st[i]
+            if(dir == 'up'):
+                row = row- 1
+            if(dir == 'down'):
+                row = row + 1
+            print(row, dir, st[i], i, res)
+        for r in list:
+            print("r: ", r)
+            res += r
+        return res
 c = Solution()
-c.convert("chey", 4)
+res = c.convert("thisistheway", 3)
+print("res: ", res)
