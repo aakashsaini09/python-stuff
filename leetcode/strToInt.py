246bed2d009c8230786cd6a7a5101de2bdb98e80
aakashsaini09 <aakashsaini948585@gmail.com>
2026-07-06 15:45:15 +0530

    sol that passed more edge cases

--- leetcode/strToInt.py
+++ leetcode/strToInt.py
@@ -44,7 +44,82 @@ class Solution:
             i = i - 1
         return int(ans)
     
+
+    def sol(self, s: str) -> int:
+        a = s.replace(" ", "")
+        ans = ''
+        i = 0
+        if(len(a) == 1):
+            return int(a)
+        if(a[0] == '-'):
+            if(len(a) == 2):
+                return int(a)
+            z = a.split('-', 1)
+            temp = z[1]
+            while(i != len(temp)):
+                if(ans == '' and temp[i] == '0'):
+                    i = i + 1
+                    continue
+                if(temp[i].isalpha()):
+                    i = i + 1
+                    continue
+                ans = ans + temp[i]
+                i = i + 1
+            # res = self.rev(ans)
+            temp = ''
+            i = len(ans)
+            while(i != 0):
+                temp = temp + ans[i - 1]
+                i = i - 1
+            i = int(temp)
+            r = i - i - i
+            return r
+        # *********************************************************************************
+        if(a[0] == '+'):
+            if(len(a) == 2):
+                return int(a)
+            z = a.split('+', 1)
+            temp = z[1]
+            while(i != len(temp)):
+                if(ans == '' and temp[i] == '0'):
+                    i = i + 1
+                    continue
+                if(temp[i].isalpha()):
+                    i = i + 1
+                    continue
+                ans = ans + temp[i]
+                i = i + 1
+            # res = self.rev(ans)
+            temp = ''
+            i = len(ans)
+            while(i != 0):
+                temp = temp + ans[i - 1]
+                i = i - 1
+            return int(temp)
+        
+        while(i != len(a)):
+            if(a[i].isalpha()):
+                i = i + 1
+                continue
+            ans = ans + a[i]
+            i = i + 1
+        # res = self.rev(ans)
+        temp = ''
+        i = len(ans)
+        while(i != 0):
+            temp = temp + ans[i - 1]
+            i = i - 1
+        
+        return int(temp)
+    def rev(self, val):
+        ab = ''
+        i = len(val)
+        while(i != 0):
+            ab = ab + val[i - 1]
+            i = i - 1
+        print("final ans: ", ab)
+        return int(ab)
    
 a = Solution()
-result = a.myAtoi("   -042")
+result = a.sol("   -0043")
 print(type(result), result)
