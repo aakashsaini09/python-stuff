485efb590df14310945bc71250e6bb004a44a2bb
aakashsaini09 <aakashsaini948585@gmail.com>
2026-07-08 15:22:45 +0530

    romanToInt prob Solved

--- leetcode/palandrom.py
+++ leetcode/palandrom.py
@@ -31,7 +31,7 @@ class Solution2:
     def isMatch(self, s: str, p: str) -> bool:
         ans = False
         return ans
-class Solution:
+class Solution3:
     def maxArea(self, height: List[int]) -> int:
         start = 0
         end = len(height) - 1
@@ -57,6 +57,66 @@ class Solution:
                 start = start + 1
                 continue
         return area
+    
+# 13 Roman number to intc conver
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        i = len(s)
+        rev = ''
+        pre: int = 0
+        if(len(s) == 0):
+            return 0
+        while(i != 0):
+            rev= rev + s[i - 1]
+            i = i - 1
+        obj = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        i = 0
+        while(i != len(rev)):
+            current = obj[rev[i]]
+            print(pre, current, ans)
+            if(pre > current):
+                value = pre - current
+                ans = ans - pre + value
+                print("Value", value)
+                pre = current
+                print("#####################")
+            else:
+                if(pre != 0):
+                    print(current)
+                    pre = current
+                    ans = ans + current
+                else:
+                    pre = current
+                    ans = ans + current
+                print("*************************")
+            i = i + 1
+        return ans
+
+    # def rev(self, val):
+    #     ab = ''
+    #     i = len(val)
+    #     while(i != 0):
+    #         ab = ab + val[i - 1]
+    #         i = i - 1
+    #     return ab
+   
 a = Solution()
-ans = a.maxArea([])
+ans = a.romanToInt("LXX")
 print("Ans: ", ans)
