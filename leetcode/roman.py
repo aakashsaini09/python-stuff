3a2ff9b33cc80addff7b4c597b296a59c72f6639
aakashsaini09 <aakashsaini948585@gmail.com>
2026-07-11 18:33:31 +0530

    brudeForce solution for 3 digit sum prob

--- leetcode/roman.py
+++ leetcode/roman.py
@@ -65,6 +65,43 @@ class Solution:
             #     print("J is: ", j)
             #     if(j[i] == strs[0][i]):
             #         print("match: ", j[i], str[0][i])
+
+
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        ans = nums[0] + nums[1] + nums[2]
+        i = 0
+        while i != len(nums) - 2:
+            j = i + 1
+            while j != len(nums) - 1:
+                k = j + 1
+                while k != len(nums):
+                    current:float = nums[i] + nums[j] + nums[k]
+                    new = abs(current - target)
+                    old = abs(ans - target)
+                    if(new < old):
+                        ans = current
+                    print(current, nums[i], nums[j], nums[k])
+                    k = k + 1
+                j = j + 1
+            i = i + 1
+        return ans
+    
+
+
+
+
+        # for i in nums:
+        #     for j in nums:
+        #         if(j == nums[0]):
+        #             print("skip for ", j)
+        #             continue
+        #         print("J: ", j)
+        #         for k in nums:
+        #             if(k == nums[0] or k == nums[1]):
+        #                 print("skip for ", k)
+        #                 continue
+        #             print("k: ", k)
+        # return ans
 a = Solution()
-ans = a.longestCommonPrefix(["ab","a"])
+ans = a.threeSumClosest([-43,57,-71,47,3,30,-85,6,60,-59,0,-46,-40,-73,53,68,-82,-54,88,73,20,-89,-22,39,55,-26,95,-87,-57,-86,28,-37,43,-27,-24,-88,-35,82,-3,39,-85,-46,37,45,-24,35,-49,-27,-96,89,87,-62,85,-44,64,78,14,59,-55,-10,0,98,50,-75,11,97,-72,85,-68,-76,44,-12,76,76,8,-75,-64,-57,29,-24,27,-3,-45,-87,48,10,-13,17,94,-85,11,-42,-98,89,97,-66,66,88,-89,90,-68,-62,-21,2,37,-15,-13,-24,-23,3,-58,-9,-71,0,37,-28,22,52,-34,24,-8,-20,29,-98,55,4,36,-3,-9,98,-26,17,82,23,56,54,53,51,-50,0,-15,-50,84,-90,90,72,-46,-96,-56,-76,-32,-8,-69,-32,-41,-56,69,-40,-25,-44,49,-62,36,-55,41,36,-60,90,37,13,87,66,-40,40,-35,-11,31,-45,-62,92,96,8,-4,-50,87,-17,-64,95,-89,68,-51,-40,-85,15,50,-15,0,-67,-55,45,11,-80,-45,-10,-8,90,-23,-41,80,19,29,7], 255)
 print("Ans: ", ans)
