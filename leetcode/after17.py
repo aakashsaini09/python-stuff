07014e298a4246039955a8b261a9314543ca3487
aakashsaini09 <aakashsaini948585@gmail.com>
2026-07-12 15:23:44 +0530

    prob 18th solved

---
+++ leetcode/after17.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        return []
+
+
+c = Solution()
+ans = c.letterCombinations(21)
+print(ans)
